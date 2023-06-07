6436d8a1f90506c76aae65c66f3c6888bcb2b59c
Yevgen <evgeniygal@gmail.com>
2023-06-07 21:13:25 +0300

    Initial commit

---
+++ src/main/java/edu/goit/Module12SpringCoreApplication.java
@@ -0,0 +1,13 @@
+package edu.goit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module12SpringCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module12SpringCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/goit/Module12SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.goit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module12SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
